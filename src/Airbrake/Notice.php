37818b04dc9b44fb8d7ac54f7460aafd1e1ff484
Drew Butler <nodrew@gmail.com>
2011-11-17 15:52:41 -0500

    Fixed a bug that was causing the json_encode to choke while encountering certain objects. May need to be revisited.

--- src/Airbrake/Notice.php
+++ src/Airbrake/Notice.php
@@ -6,10 +6,10 @@ use SimpleXMLElement;
 /**
  * Airbrake notice class.
  *
- * @package		Airbrake
- * @author		Drew Butler <drew@abstracting.me>
- * @copyright	(c) 2011 Drew Butler
- * @license		http://www.opensource.org/licenses/mit-license.php
+ * @package     Airbrake
+ * @author      Drew Butler <drew@abstracting.me>
+ * @copyright   (c) 2011 Drew Butler
+ * @license     http://www.opensource.org/licenses/mit-license.php
  */
 class Notice extends Record
 {
@@ -36,65 +36,65 @@ class Notice extends Record
      */
     public function toXml(Configuration $configuration)
     {
-		$doc = new SimpleXMLElement('<notice />');
-		$doc->addAttribute('version', Version::API);
-		$doc->addChild('api-key', $configuration->get('apiKey'));
+        $doc = new SimpleXMLElement('<notice />');
+        $doc->addAttribute('version', Version::API);
+        $doc->addChild('api-key', $configuration->get('apiKey'));
 
-		$notifier = $doc->addChild('notifier');
-		$notifier->addChild('name', Version::NAME);
-		$notifier->addChild('version', Version::NUMBER);
-		$notifier->addChild('url', Version::APP_URL);
+        $notifier = $doc->addChild('notifier');
+        $notifier->addChild('name', Version::NAME);
+        $notifier->addChild('version', Version::NUMBER);
+        $notifier->addChild('url', Version::APP_URL);
 
-		$env = $doc->addChild('server-environment');
-		$env->addChild('project-root', $configuration->get('projectRoot'));
-		$env->addChild('environment-name', $configuration->get('environmentName'));
+        $env = $doc->addChild('server-environment');
+        $env->addChild('project-root', $configuration->get('projectRoot'));
+        $env->addChild('environment-name', $configuration->get('environmentName'));
 
-		$error = $doc->addChild('error');
-		$error->addChild('class', $this->errorClass);
-		$error->addChild('message', $this->errorMessage);
+        $error = $doc->addChild('error');
+        $error->addChild('class', $this->errorClass);
+        $error->addChild('message', $this->errorMessage);
 
-		if (count($this->backtrace) > 0) {
-    		$backtrace = $error->addChild('backtrace');
-    		foreach ($this->backtrace as $entry) {
-    			$line = $backtrace->addChild('line');
-    			$line->addAttribute('file', isset($entry['file']) ? $entry['file'] : '');
-    			$line->addAttribute('number', isset($entry['line']) ? $entry['line'] : '');
-    			$line->addAttribute('method', isset($entry['function']) ? $entry['function'] : '');
-    		}
-	    }
+        if (count($this->backtrace) > 0) {
+            $backtrace = $error->addChild('backtrace');
+            foreach ($this->backtrace as $entry) {
+                $line = $backtrace->addChild('line');
+                $line->addAttribute('file', isset($entry['file']) ? $entry['file'] : '');
+                $line->addAttribute('number', isset($entry['line']) ? $entry['line'] : '');
+                $line->addAttribute('method', isset($entry['function']) ? $entry['function'] : '');
+            }
+        }
 
-		$request = $doc->addChild('request');
-		$request->addChild('url', $configuration->get('url'));
-		$request->addChild('component', $configuration->get('component'));
-		$request->addChild('action', $configuration->get('action'));
+        $request = $doc->addChild('request');
+        $request->addChild('url', $configuration->get('url'));
+        $request->addChild('component', $configuration->get('component'));
+        $request->addChild('action', $configuration->get('action'));
 
-		$this->array2Node($request, 'params', $configuration->getParamters());
-		$this->array2Node($request, 'session', $configuration->get('sessionData'));
-		$this->array2Node($request, 'cgi-data', $configuration->get('serverData'));
+        $this->array2Node($request, 'params', $configuration->getParamters());
+        $this->array2Node($request, 'session', $configuration->get('sessionData'));
+        $this->array2Node($request, 'cgi-data', $configuration->get('serverData'));
 
-		return $doc->asXML();
+        return $doc->asXML();
     }
 
-	/**
-	 * Add a Airbrake var block to an XML node.
-	 *
-	 * @param SimpleXMLElement $parentNode
-	 * @param string $key
-	 * @param array $params
-	 **/
-	protected function array2Node($parentNode, $key, $params)
-	{
-		if (count($params) == 0) {
-		    return;
-	    }
+    /**
+     * Add a Airbrake var block to an XML node.
+     *
+     * @param SimpleXMLElement $parentNode
+     * @param string $key
+     * @param array $params
+     **/
+    protected function array2Node($parentNode, $key, $params)
+    {
+        if (count($params) == 0) {
+            return;
+        }
 
-		$node = $parentNode->addChild($key);
-		foreach ($params as $key => $value) {
-		    if (is_array($value)) {
-		        $value = json_encode($value);
+        $node = $parentNode->addChild($key);
+        foreach ($params as $key => $value) {
+            if (is_array($value) || is_object($value)) {
+                $value = json_encode((array) $value);
             }
-    
-			$node->addChild('var', $value)->addAttribute('key', $key);
-		}
-	}
+
+            $node->addChild('var', $value)->addAttribute('key', $key);
+        }
+    }
 }
