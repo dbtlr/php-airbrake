2ab3a98ddfbca3b0926a9dd707c0673092c48a74
Olivier Le Floch <olivier@le-floch.fr>
2012-08-13 14:24:12 -0700

    Grammar

--- src/Airbrake/Configuration.php
+++ src/Airbrake/Configuration.php
@@ -92,7 +92,8 @@ class Configuration extends Record
     public function verify()
     {
         if (!$this->apiKey) {
-            throw new AirbrakeException('Cannot initialize the Airbrake client without an ApiKey being set to the configuration.');
+            throw new AirbrakeException(
+                'Cannot initialize the Airbrake client without an ApiKey being set in the configuration.');
         }
     }
 }
