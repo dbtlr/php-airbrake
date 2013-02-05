aa75c988d5a9fe4ff6c2f2a073fcb411e117375c
Drew Butler <drew@nodrew.com>
2013-02-05 15:39:52 -0500

    Fix the shutdown message to not be so verbose, to prevent Airbrake from grouping them #14

--- src/Airbrake/EventHandler.php
+++ src/Airbrake/EventHandler.php
@@ -167,8 +167,7 @@ class EventHandler
             return;
         }
 
-        $message = 'It looks like we may have shutdown unexpectedly. Here is the error '
-                 . 'we saw while closing up: %s  File: %s  Line: %d';
+        $message = '[SHUTDOWN] %s  File: %s  Line: %d';
 
         $message = sprintf($message, $error['message'], $error['file'], $error['line']);
 
