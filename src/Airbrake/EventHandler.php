ba3f194b68dc39f887fc2f4c9c6e6da58fe551db
James Turner <james.turner.phpninja@gmail.com>
2014-04-23 11:06:30 +0100

    Corrected undefined variable in shutdown function causing recursive error handling behaviour.

--- src/Airbrake/EventHandler.php
+++ src/Airbrake/EventHandler.php
@@ -174,6 +174,6 @@ class EventHandler
             )
         );
 
-        $this->airbrakeClient->notifyOnError('[Improper Shutdown] '.$message, $backtrace);
+        $this->airbrakeClient->notifyOnError('[Improper Shutdown] '.$error['message'], $backtrace);
     }
 }
